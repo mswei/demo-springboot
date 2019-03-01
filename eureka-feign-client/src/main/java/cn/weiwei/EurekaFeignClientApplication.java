a535556ada75a11b6d61846e6f3881f69e06c4c0
weiwei <weiwei.ms@gmail.com>
2019-03-01 22:37:00 +0800

    修改

--- eureka-feign-client/src/main/java/cn/weiwei/EurekaFeignClientApplication.java
+++ eureka-feign-client/src/main/java/cn/weiwei/EurekaFeignClientApplication.java
@@ -1,19 +1,34 @@
 package cn.weiwei;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import config.FeignConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @EnableEurekaClient
 @EnableFeignClients
 @Import(FeignConfig.class)
+@EnableHystrixDashboard
 @SpringBootApplication
 public class EurekaFeignClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaFeignClientApplication.class, args);
     }
+
+    // 不配置会无法访问hystrix.stream
+    @Bean(name = "hystrixRegistrationBean")
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet(), "/actuator/hystrix.stream");
+        registration.setName("hystrixServlet");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+
 }
