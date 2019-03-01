b30abfd956192c0c071df67e297ca04be21d1dde
weiwei <weiwei.ms@gmail.com>
2019-03-01 17:52:08 +0800

    Hystrix Dashboard

--- eureka-ribbon-client/src/main/java/cn/weiwei/EurekaRibbonClientApplication.java
+++ eureka-ribbon-client/src/main/java/cn/weiwei/EurekaRibbonClientApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
 public class EurekaRibbonClientApplication {
 
