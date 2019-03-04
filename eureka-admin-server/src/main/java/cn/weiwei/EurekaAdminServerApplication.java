4b36a763edb7868aad7f97347a29d3061ec48999
weiwei <weiwei.ms@gmail.com>
2019-03-05 00:02:05 +0800

    添加验证

--- eureka-admin-server/src/main/java/cn/weiwei/EurekaAdminServerApplication.java
+++ eureka-admin-server/src/main/java/cn/weiwei/EurekaAdminServerApplication.java
@@ -1,17 +1,19 @@
 package cn.weiwei;
 
+import config.SecuritySecureConfig;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Import;
 
 @EnableEurekaClient
 @EnableAdminServer
+@Import(SecuritySecureConfig.class)
 @SpringBootApplication
 public class EurekaAdminServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaAdminServerApplication.class, args);
     }
-
 }
