f6b4678179bd39610d38bd939ec6014f3408cedc
weiwei <weiwei.ms@gmail.com>
2019-03-08 15:28:03 +0800

    Kafka

---
+++ kafka-client/src/main/java/cn/weiwei/KafkaClientApplication.java
@@ -0,0 +1,14 @@
+package cn.weiwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class KafkaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaClientApplication.class, args);
+    }
+}
